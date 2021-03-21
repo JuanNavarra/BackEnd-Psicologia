c897a56885c55b3df7d6bd4ec8cc4848485c923c
juannavarra25 <juannavarra25@gmail.com>
2021-03-21 17:45:46 +0100

    commnit

--- Repositorio/Repositorio/Principal/Interfaz/IPrincipalRepository.cs
+++ Repositorio/Repositorio/Principal/Interfaz/IPrincipalRepository.cs
@@ -1,7 +1,6 @@
 ﻿namespace Repositorio
 {
     using Dtos;
-    using Modelos;
     using System;
     using System.Collections.Generic;
     using System.Text;
@@ -13,37 +12,5 @@
         /// </summary>
         /// <returns></returns>
         public FaqsDto MostrarFaq();
-        /// <summary>
-        /// Guarda la seccion principal de la pagina
-        /// </summary>
-        /// <param name="principal"></param>
-        public void GuardarSeccionPrincipal(Principal principal);
-        /// <summary>
-        /// Busca el contenido de la pagina principal por el id
-        /// </summary>
-        /// <param name="descripcion"></param>
-        /// <returns></returns>
-        public Principal BuscarContenidoPrincipal(string descripcion);
-        /// <summary>
-        /// Lista el contendido principal
-        /// </summary>
-        /// <returns></returns>
-        public List<PrincipalDto> ListarContenidoPrincipal();
-        /// <summary>
-        /// Muestra el contendio principal
-        /// </summary>
-        /// <returns></returns>
-        public PrincipalDto MostrarContenidoPrincipal();
-        /// <summary>
-        /// Buscar principal por id
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public Principal BuscarPorId(int id);
-        /// <summary>
-        /// Actualiza el principal
-        /// </summary>
-        /// <param name="principal"></param>
-        public void ActualizarSeccion(Principal principal);
     }
 }
